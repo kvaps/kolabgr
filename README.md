a333c4ab558ac4872240f8ff6f921b4f47c20ac8
kvaps <kvapss@gmail.com>
2015-10-09 11:43:35 +0300

    kolabgr updated

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 This script generates a list of groups and members from Kolab's LDAP for Prosody's mod_groups
 
 ### Usage
+Now script partly uses [mod_lib_ldap](https://modules.prosody.im/mod_lib_ldap.html) config, so be sure in the presence of [kolabgr section](https://github.com/kvaps/kolabgr/blob/master/ldap.cfg.lua#L6-L10) in ldap.cfg.lua
 
-    /usr/bin/lua /bin/kolabgr.lua > /etc/prosody/groups.txt
-
+Run:
+```bash
+/usr/bin/lua /etc/prosody/kolabgr.lua > /etc/prosody/groups.txt
+```
 add this to your crontab
--- kolabgr.lua
+++ kolabgr.lua
@@ -5,54 +5,63 @@
 
 require "lualdap"
 
--- Configuration
-config = {
-    ldapserver = "localhost",
-    binddn = "cn=Directory Manager",
-    passwd = "pass",
-    user = {
-        basedn = "ou=People,dc=example,dc=com",
-        filter = "(objectClass=mailrecipient)",
-    },
-    groups = {
-        basedn = "ou=Groups,dc=example,dc=com",
-        filter = "(objectClass=groupofuniquenames)",  -- Simple groups
---        filter = "(objectClass=kolabgroupofuniquenames)",  -- only Kolab groups
-    },
-}
+-- Load configuration
+dofile "/etc/prosody/ldap.cfg.lua"
 
-ld = assert (lualdap.open_simple (config.ldapserver, config.binddn, config.passwd))
+ld = assert (lualdap.open_simple (ldap.hostname, ldap.bind_dn, ldap.bind_password))
 
 users = {}
 groups = {}
 
 -- Writing LDAP to tables
-for dn, attribs in ld:search { base = config.user.basedn, scope = 'onelevel', filter = config.user.filter } do users[dn]=attribs end
-for dn, attribs in ld:search { base = config.groups.basedn, scope = 'onelevel', filter = config.groups.filter } do groups[dn]=attribs end
+for dn, attribs in ld:search { base = ldap.user.basedn, scope = 'onelevel', filter = ldap.user.filter } do users[dn]=attribs end
+for dn, attribs in ld:search { base = ldap.groups.basedn, scope = 'onelevel', filter = ldap.groups.filter } do groups[dn]=attribs end
+
 
 -- Search users by groups
-for groupname, val in pairs (groups) do -- Take groups
-    if val.cn ~= "All" then    -- If groupname is not "All"
+function print_user(val)
+    if ldap.user.usernamefield == "mail" then
+        print (val.mail .. "=" .. val.displayName)
+    else
+        print (val.uid .. "@" .. ldap.kolabgr.domain .. "=" .. val.displayName)
+    end
+end
+
+function print_group(val)
+    if ldap.kolabgr.show_all_groups == true then
+        io.write ("[+" .. val.cn .. "]\n")
+    else
         io.write ("[" .. val.cn .. "]\n")
+    end
         if val.uniqueMember then            -- If not empty
-            if type(val.uniqueMember) == 'table' then
-                for gr, member in pairs (val.uniqueMember) do -- Take uniqueMember parameter of group
-                    for usercn, val in pairs (users) do       -- Take cn paramete of user
-                        if member == usercn then
-                            io.write (val.mail .. "=" .. val.displayName .. "\n")
-                        end
-                    end
-                end
+             if type(val.uniqueMember) == 'table' then
+                 for gr, member in pairs (val.uniqueMember) do -- Take uniqueMember parameter of group
+                      for usercn, val in pairs (users) do       -- Take cn paramete of user
+                          if member == usercn then
+                              print_user(val)
+                          end
+                      end
+                 end
             else    -- If only one
-                member=val.uniqueMember;
-                for usercn, val in pairs (users) do       -- Take cn paramete of user
-                    if member == usercn then
-                        io.write (val.mail .. "=" .. val.displayName .. "\n")
-                    end
-                end
+                 member=val.uniqueMember;
+                 for usercn, val in pairs (users) do       -- Take cn paramete of user
+                      if member == usercn then
+                          print_user()
+                      end
+                 end
             end
+        end
+end
+
+for group, val in pairs (groups) do -- Take groups
+    for id, igngroup in pairs (ldap.kolabgr.not_show_groups) do -- Remove ignoring groups
+        if val.cn == igngroup then 
+            groups[group] = nil
         end
     end
 end
 
+for group, val in pairs (groups) do -- Take groups
+    print_group(val)
+end
 -- vim:sts=4 sw=4
